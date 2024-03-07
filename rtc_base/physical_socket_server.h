984758475f86fd2fdd51c5dc04c26cf748883a1c
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-03-07 09:08:36 +0000

    Applied patches to webrtc from Chromium 115.0.5790.110 FreeBSD port.

--- rtc_base/physical_socket_server.h
+++ rtc_base/physical_socket_server.h
@@ -14,7 +14,7 @@
 #include "api/units/time_delta.h"
 
 #if defined(WEBRTC_POSIX)
-#if defined(WEBRTC_LINUX)
+#if defined(WEBRTC_LINUX) && !defined(WEBRTC_BSD)
 // On Linux, use epoll.
 #include <sys/epoll.h>
 #define WEBRTC_USE_EPOLL 1
